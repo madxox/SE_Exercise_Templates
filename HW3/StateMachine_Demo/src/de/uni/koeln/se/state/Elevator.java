e2bafba089797508c4b9366234029512451d3592
Madeleine Dormeyer <mdormeye@smail.uni-koeln.de>
2022-10-30 11:35:04 +0100

    task 3

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -5,21 +5,56 @@ public class Elevator{
 	
 	public static int current_Floor=5;
 	int dest_Floor;
-	
 	private Elv_States State = new Elv_States();
 	
-	public Elevator (int dest_floor)
-	{
-		this.dest_Floor=dest_floor;
-	System.out.println("*** Destination floor is: "+ dest_Floor);
-	arrive_atFloor();
-
-
+	public Elevator (int dest_floor) {
+		
+		this.dest_Floor = dest_floor;
+		System.out.println("*** Destination floor is: "+ dest_Floor);
+		
+		if(current_Floor < dest_Floor) {
+			go_up();
+		}
+		else if(current_Floor > dest_Floor) {
+			go_down();
+		}
+		else if(current_Floor == dest_Floor) {
+			arrive_atFloor();
+		}
+		
+		
 	}
 
 	private void arrive_atFloor() {
+		State.current_State = State.idle;
+		exit();
+	}
+	
+	private void exit() {
+		System.out.println("Elevator has reached floor "+current_Floor
+				+", you can now exit");
 		
 	}
+
+	private void go_up() {
+		State.current_State = State.moving_up;
+		while(current_Floor < dest_Floor) {
+			System.out.println("|");
+			current_Floor += 1;
+		}
+		arrive_atFloor();
+	}
+	
+	private void go_down() {
+		State.current_State = State.moving_down;
+		while(current_Floor > dest_Floor) {
+			System.out.println("|");
+			current_Floor -= 1;
+		}
+		arrive_atFloor();
+	}
+	
+	
 
 	
 }
--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
@@ -2,7 +2,16 @@ package de.uni.koeln.se.state;
 
 public class Elv_States {
 	
-	public  String Moving_up = "Moving_up";
-
-
+	public String moving_up = "Moving_up";
+	public String moving_down = "Moving_down";
+	public String idle = "Idle";
+	
+	public String current_State;
+	
+	public Elv_States() {
+		this.current_State = "";
+		
+	}
+	
+	
 }
